217da373157a1ae8cbe2915754a25f98e9dd9b00
Jan Simonek <jsimonek@dnanexus.com>
2026-08-10 11:38:23 +0200

    Fixing formatting

--- aws/src/main/java/org/apache/iceberg/aws/glue/GlueCatalog.java
+++ aws/src/main/java/org/apache/iceberg/aws/glue/GlueCatalog.java
@@ -365,16 +365,17 @@ public class GlueCatalog extends BaseMetastoreViewCatalog
   @Override
   public boolean dropTable(TableIdentifier identifier, boolean purge) {
     try {
-      String databaseName = IcebergToGlueConverter.getDatabaseName(
+      String databaseName =
+          IcebergToGlueConverter.getDatabaseName(
               identifier, awsProperties.glueCatalogSkipNameValidation());
 
       GetTableResponse getTableResponse =
-              glue.getTable(
-                      GetTableRequest.builder()
-                              .catalogId(awsProperties.glueCatalogId())
-                              .databaseName(databaseName)
-                              .name(identifier.name())
-                              .build());
+          glue.getTable(
+              GetTableRequest.builder()
+                  .catalogId(awsProperties.glueCatalogId())
+                  .databaseName(databaseName)
+                  .name(identifier.name())
+                  .build());
       if (isGlueIcebergView(getTableResponse.table())) {
         LOG.warn("dropTable({}) called but Glue table is an iceberg-view", identifier);
         return false;
@@ -453,7 +454,8 @@ public class GlueCatalog extends BaseMetastoreViewCatalog
     }
 
     if (isGlueIcebergView(fromTable)) {
-      throw new NoSuchTableException("Cannot rename %s because it is not an Iceberg table in Glue", from);
+      throw new NoSuchTableException(
+          "Cannot rename %s because it is not an Iceberg table in Glue", from);
     }
 
     // use the same Glue info to create the new table, pointing to the old metadata
